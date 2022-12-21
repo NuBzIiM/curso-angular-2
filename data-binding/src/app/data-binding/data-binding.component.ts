f377dbccc4ddf80c9d0f8dbe946cfce441d77db7
NuBzIiM <breendhonh@gmail.com>
2022-12-21 11:22:28 -0400

    event binding and style binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  //styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+      .highlight{
+        background-color: yellow;
+        front-weight: bold;
+      }
+
+    `
+  ]
 })
 export class DataBindingComponent {
 
   url: string = 'http://loiane.com'
   cursoAngular: boolean = true;
   urlImage = 'http://lorempixel.com.br/400/200/nature';
+
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
 
   getValor(){
     return 1
@@ -18,4 +32,21 @@ export class DataBindingComponent {
   getCurtirCurso(){
     return true
   }
+
+  botaoClicado(){
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: any){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
 }
